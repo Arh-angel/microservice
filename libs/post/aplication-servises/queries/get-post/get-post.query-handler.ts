ec14f6527e44af14cc67020bef414cad53faf2d9
AlexandrKh <khalex8993@gmail.com>
2023-07-06 13:26:48 +0300

    ADD: get one post query

---
+++ libs/post/aplication-servises/queries/get-post/get-post.query-handler.ts
@@ -0,0 +1,22 @@
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { GetPostQuery } from './get-post.query';
+import { PostAggregate } from '@lib/post';
+import { PostRepository } from 'libs/post/providers';
+import { Logger } from '@nestjs/common';
+
+@QueryHandler(GetPostQuery)
+export class GetPostQueryHandler
+  implements IQueryHandler<GetPostQuery, PostAggregate>
+{
+  private readonly logger = new Logger(GetPostQueryHandler.name);
+
+  constructor(private readonly postRepository: PostRepository) {}
+  async execute({ id }: GetPostQuery): Promise<PostAggregate> {
+    const existPost = await this.postRepository.findOne(id).catch((err) => {
+      this.logger.error(err);
+      return null as PostAggregate;
+    });
+
+    return existPost;
+  }
+}

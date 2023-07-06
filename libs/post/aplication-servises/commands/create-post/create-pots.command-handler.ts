c6f13cd801d05d53791e5caaa6c348b4b6e70893
AlexandrKh <khalex8993@gmail.com>
2023-07-06 11:16:50 +0300

    ADD: update post handler

--- libs/post/aplication-servises/commands/create-post/create-pots.command-handler.ts
+++ libs/post/aplication-servises/commands/create-post/create-pots.command-handler.ts
@@ -1,5 +1,22 @@
-import { CreatePostDto } from '../dto';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { PostAggregate } from '@lib/post';
+import { PostRepository } from 'libs/post/providers';
+import { BadRequestException } from '@nestjs/common';
+import { CreatePostCommand } from './create-post.command';
 
-export class CreatePostCommand {
-  constructor(public readonly post: CreatePostDto) {}
+@CommandHandler(CreatePostCommand)
+export class CreatePostCommandHandler
+  implements ICommandHandler<CreatePostCommand, PostAggregate>
+{
+  constructor(private readonly postRepository: PostRepository) {}
+  async execute({ post }: CreatePostCommand): Promise<PostAggregate> {
+    const postAggregate = PostAggregate.create(post);
+    const createPost = await this.postRepository
+      .save(postAggregate)
+      .catch((err) => {
+        throw new BadRequestException(err);
+      });
+
+    return createPost;
+  }
 }

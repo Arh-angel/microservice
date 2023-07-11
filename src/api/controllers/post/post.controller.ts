06c78407f5c428a53a4fec0c1e2dbd6f8410ad38
AlexandrKh <khalex8993@gmail.com>
2023-07-11 13:01:01 +0300

    ADD:  controller for get all posts

--- src/api/controllers/post/post.controller.ts
+++ src/api/controllers/post/post.controller.ts
@@ -6,11 +6,16 @@ import {
   Param,
   ParseUUIDPipe,
   Post,
+  Query,
   UseGuards,
 } from '@nestjs/common';
 import { CreatePostDto } from './dto';
 import { CurrentUser, ICurrentUser, Pablic } from '@lib/auth';
 import { JwtGuard } from '@lib/auth/guards/jwt.guard';
+import { PaginationDto } from '@lib/shared/dto';
+import { plainToInstance } from 'class-transformer';
+import { ResponseWithPagination } from '@lib/shared';
+import { PostAggregate } from '@lib/post';
 
 @UseGuards(JwtGuard)
 @Controller('post')
@@ -32,5 +37,22 @@ export class PostController {
   @Get(':id')
   getPost(@Param('id', ParseUUIDPipe) id: string) {
     return this.postFacade.queries.getPost(id);
+  }
+
+  @Pablic()
+  @Get()
+  async getPosts(
+    @Query() paginationDto: PaginationDto,
+  ): Promise<ResponseWithPagination<PostAggregate>> {
+    const pagination = plainToInstance(PaginationDto, paginationDto);
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //@ts-ignore
+    const [data, count] = await this.postFacade.queries.getPosts(pagination);
+
+    return {
+      ...pagination,
+      data,
+      total: count,
+    };
   }
 }

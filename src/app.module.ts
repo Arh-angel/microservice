bfd389188fbc8a48492b00b967f1e79721931d94
AlexandrKh <khalex8993@gmail.com>
2023-07-12 14:05:23 +0300

    ADD: amqp module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { SharedModule } from '@lib/shared';
 import { Module } from '@nestjs/common';
 import { DomainsModule } from './domains/domains.module';
 import { ApiModule } from './api';
+import { ChannelsModule } from './channels/channels.module';
 
 @Module({
-  imports: [ProvidersModule, SharedModule, DomainsModule, ApiModule],
+  imports: [ProvidersModule, SharedModule, DomainsModule, ApiModule, ChannelsModule],
 })
 export class AppModule {}
